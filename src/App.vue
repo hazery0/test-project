f19f5c897ced917b911d9e175633c5272b09bfad
hazery0 <1193553488@qq.com>
2025-11-05 17:00:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <h1 id="htitle">{{ message  }}</h1> -->
+    <!-- <test-button></test-button>> -->
+
+    <!-- <user-view></user-view> -->
+    <!-- <question-view></question-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import QuestionView from './views/element/QuestionView.vue';
+// import TestButton from './views/element/TestButton.vue';
+// import UserView from './views/element/UserView.vue';
+
+export default {
+  // components: { QuestionView },
+  data() {
+    return {
+      message: 'Hello Vue',
+      //num: 1,
+    };
+  },
+  methods: {
+
+  },
+}
+</script>
---
+++ src/views/element/TestButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+
+        <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字">
+        </el-input-number>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello Vue',
+            num: 1,
+        };
+    },
+    methods: {
+        handleChange(value) {
+            console.log('当前值：', value);
+        },
+    },
+}
+</script>
+
+<style></style>
